af65b6ac4da12352b911fcc2e12b1f0092148c97
Erik White <erikwhite@Eriks-MacBook-Pro.local>
2019-08-25 16:28:23 +0300

    Basic program structure

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mosey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program start");
+
+        }
+    }
+}
