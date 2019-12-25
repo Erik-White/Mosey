5b3c5b3ec89137605bb97dcbe770d6b1076a5687
Erik White <26148654+Erik-White@users.noreply.github.com>
2019-12-25 02:29:08 +0200

    Add BoolToVisibilityConverter that allows the default visibility values to be set

--- Mosey/ViewModels/Converters/Converters.cs
+++ Mosey/ViewModels/Converters/Converters.cs
@@ -101,6 +101,43 @@ namespace Mosey.ViewModels.Converters
         }
     }
 
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public sealed class BoolToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Returns a <c>Visibility</c> from a <c>Boolean</c> value
+        /// Allows the default <c>Visibility to be specified</c>
+        /// </summary>
+        /// <param name="value"><c>Boolean</c></param>
+        /// <returns>Returns a <c>Visibility</c></returns>
+        public Visibility TrueValue { get; set; }
+        public Visibility FalseValue { get; set; }
+
+        public BoolToVisibilityConverter()
+        {
+            TrueValue = Visibility.Visible;
+            FalseValue = Visibility.Collapsed;
+        }
+
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+                return null;
+            return (bool)value ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if (Equals(value, TrueValue))
+                return true;
+            if (Equals(value, FalseValue))
+                return false;
+            return null;
+        }
+    }
+
     public class BoolToValueConverter<T> : MarkupExtension, IValueConverter
     {
         /// <summary>
