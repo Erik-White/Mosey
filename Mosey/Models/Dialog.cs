7c61b8112630f8f801f85a194177f22db52a676c
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-01-04 13:51:14 +0200

    Add a dialog window service to handle directory selection

---
+++ Mosey/Models/Dialog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mosey.Models
+{
+    public interface IDialog
+    {
+        string Title { get; set; }
+    }
+
+    public interface IFileBrowserDialog : IDialog
+    {
+        string InitialDirectory { get; set; }
+        Environment.SpecialFolder RootDirectory { get; set; }
+        bool? ShowDialog();
+    }
+
+    public interface IFolderBrowserDialog : IFileBrowserDialog
+    {
+        string SelectedPath { get; set; }
+    }
+}
