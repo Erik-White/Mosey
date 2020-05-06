3025c0cc94025e1c9d4888c15685068215688d00
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-05-06 17:33:15 +0300

    Add UIServices dependency injector facade

---
+++ Mosey/Services/UIServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mosey.Models.Dialog;
+
+namespace Mosey.Services
+{
+    /// <summary>
+    /// Aggregate of common UI services
+    /// </summary>
+    public class UIServices
+    {
+        public IDialogManager DialogManager { get; }
+        public IFolderBrowserDialog FolderBrowserDialog { get; }
+
+        public UIServices(
+            IDialogManager dialogManager,
+            IFolderBrowserDialog folderBrowserDialog
+            )
+        {
+            DialogManager = dialogManager;
+            FolderBrowserDialog = folderBrowserDialog;
+        }
+    }
+}
