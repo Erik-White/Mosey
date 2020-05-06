7161750b2a3fd0342493c13c51a8efb074cdb8f8
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-05-06 14:37:07 +0300

    Add Dialog namespace and interface implementation. Move existing dialog classes to new namespace.

---
+++ Mosey/Models/Dialog/Dialog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mosey.Models.Dialog
+{
+    /// <summary>
+    /// Base dialog interface
+    /// </summary>
+    public interface IDialog
+    {
+        string Title { get; set; }
+    }
+}
---
+++ Mosey/Models/Dialog/DialogInstance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mosey.Models.Dialog
+{
+    public interface IDialogInstance : IDialog
+    {
+        IDialogSettings DialogSettings { get; }
+        string Message { get; set; }
+    }
+}
