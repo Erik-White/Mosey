51d1bf529ec7eada07b6165570182dfedae34331
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-05-21 01:52:39 +0300

    Move ImageFormat extension methods to Services.Imaging namespace

---
+++ Mosey/Services/Imaging/Extensions/ImageFormatExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing.Imaging;
+
+namespace Mosey.Services.Imaging.Extensions
+{
+    public static class ImageFormatExtensions
+    {
+        /// <summary>
+        /// Convert a file extension string to an <see cref="ScanningDevice.ImageFormat"/> <see cref="Enum"/>.
+        /// </summary>
+        /// <param name="value">An <see cref="ScanningDevice.ImageFormat"/> instance</param>
+        /// <param name="imageFormatStr">A file extension <see cref="string"/></param>
+        /// <returns>An <see cref="ScanningDevice.ImageFormat"/> <see cref="Enum"/></returns>
+        public static ScanningDevice.ImageFormat FromString(this ScanningDevice.ImageFormat value, string imageFormatStr)
+        {
+            if (Enum.TryParse(imageFormatStr, ignoreCase: true, out value))
+            {
+                return value;
+            }
+            else
+            {
+                throw new ArgumentException($"{imageFormatStr} is not a supported image file extension.");
+            }
+        }
+
+        /// <summary>
+        /// Convert to a <see cref="ImageFormat"/> instance.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>A <see cref="ImageFormat"/> instance</returns>
+        public static ImageFormat ToDrawingImageFormat(this ScanningDevice.ImageFormat value)
+        {
+            return (ImageFormat)typeof(ImageFormat)
+                    .GetProperty(value.ToString(), System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.IgnoreCase)
+                    .GetValue(null);
+        }
+    }
+}
--- Mosey/Services/ScanningDevice.cs
+++ Mosey/Services/ScanningDevice.cs
@@ -653,37 +653,4 @@ namespace Mosey.Services
             return MemberwiseClone();
         }
     }
-
-    public static class ImageFormatExtensions
-    {
-        /// <summary>
-        /// Convert a file extension string to an <see cref="ScanningDevice.ImageFormat"/> <see cref="Enum"/>.
-        /// </summary>
-        /// <param name="value">An <see cref="ScanningDevice.ImageFormat"/> instance</param>
-        /// <param name="imageFormatStr">A file extension <see cref="string"/></param>
-        /// <returns>An <see cref="ScanningDevice.ImageFormat"/> <see cref="Enum"/></returns>
-        public static ScanningDevice.ImageFormat FromString(this ScanningDevice.ImageFormat value, string imageFormatStr)
-        {
-            if (Enum.TryParse(imageFormatStr, ignoreCase: true, out value))
-            {
-                return value;
-            }
-            else
-            {
-                throw new ArgumentException($"{imageFormatStr} is not a supported image file extension.");
-            }
-        }
-
-        /// <summary>
-        /// Convert to a <see cref="System.Drawing.Imaging.ImageFormat"/> instance.
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns>A <see cref="System.Drawing.Imaging.ImageFormat"/> instance</returns>
-        public static System.Drawing.Imaging.ImageFormat ToDrawingImageFormat(this ScanningDevice.ImageFormat value)
-        {
-            return (System.Drawing.Imaging.ImageFormat)typeof(System.Drawing.Imaging.ImageFormat)
-                    .GetProperty(value.ToString(), System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.IgnoreCase)
-                    .GetValue(null);
-        }
-    }
 }
