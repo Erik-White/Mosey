69554c8817a3847c5fc8904d8ce2b1ebfcf18879
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-05-21 14:00:24 +0300

    Ensure correct image encoding (#20)
    Images are currently saved with TIFF file extensions but retain their original BMP encoding.
    
    - Ensure images are saved to disk with correct encoding
    - Add extension methods for easier conversion between image byte arrays and Image instances
    - Convert image byte arrays to a format other than BMP to reduce memory overhead

---
+++ Mosey/Services/Imaging/Extensions/Utilities.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Mosey.Services.Imaging.Extensions
+{
+    internal static class Utilities
+    {
+        /// <summary>
+        /// Add an <see cref="EncoderParameter"/> to a list, or update the existing value if already present
+        /// </summary>
+        /// <param name="value">A list of <see cref="EncoderParameter"/>s</param>
+        /// <param name="encoderParameter">An <see cref="EncoderParameter"/> instance to add or update</param>
+        /// <returns>A list where the <see cref="EncoderParameter"/> has either replaced an existing value or been appended</returns>
+        internal static List<EncoderParameter> AddOrUpdate(this List<EncoderParameter> value, EncoderParameter encoderParameter)
+        {
+            // Check if the encoder is already present in the list
+            int paramIndex = value.FindIndex(e => e != null && e.Encoder == encoderParameter.Encoder);
+            if (paramIndex >= 0)
+            {
+                // Update the existing value
+                value[paramIndex]?.Dispose();
+                value[paramIndex] = encoderParameter;
+            }
+            else
+            {
+                // Otherwise add it to the list
+                value.Add(encoderParameter);
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Create a <see cref="MemoryStream"/> from an <see cref="Image"/> instance.
+        /// The <see cref="Image"/> is converted to the specified format and encoding.
+        /// </summary>
+        /// <param name="value">An <see cref="Image"/> instance</param>
+        /// <param name="format">The format for the image</param>
+        /// <param name="encoderParameters">A collection of <see cref="EncoderParameter"/>s to set image quality, colour depth etc</param>
+        /// <returns>An <see cref="Image"/> <see cref="MemoryStream"/> in the specified format and encoding</returns>
+        internal static MemoryStream ToMemoryStream(this Image value, ImageFormat format, EncoderParameters encoderParameters = null)
+        {
+            using (var ms = new MemoryStream())
+            {
+                value.Save(ms, format.CodecInfo(), encoderParameters);
+
+                return ms;
+            }
+        }
+    }
+}
