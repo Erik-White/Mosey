9fbb3df8a9fffc7a587c90ea252b1459bdb8bb07
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-06-24 23:37:45 +0300

    Add ScanningDevices tests, implementing setup with AutoFixture

--- MoseyTests/Services/Imaging/ScanningDevicesTests.cs
+++ MoseyTests/Services/Imaging/ScanningDevicesTests.cs
@@ -1,17 +1,23 @@
-﻿using AutoFixture;
-using AutoFixture.AutoMoq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
-using Mosey.Services.Imaging;
 using MoseyTests.Extensions;
+using Moq;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using DNTScanner.Core;
+using Mosey.Models;
 
 namespace Mosey.Services.Imaging.Tests
 {
     [TestClass()]
     public class ScanningDevicesTests
     {
+        private int CollectionMockCapacity { get; set; } = 5;
+
         private IFixture _fixture;
 
         [TestInitialize]
@@ -21,93 +27,226 @@ namespace Mosey.Services.Imaging.Tests
 
             // Create and configure return values for SystemDevices
             var systemDevicesMock = _fixture.FreezeMoq<ISystemDevices>();
-            /*
+            // Use concrete class otherwise cast from interface proxy type fails
+            var scannerDevices = new List<ScanningDevice>();
+            _fixture.AddManyTo(scannerDevices, CollectionMockCapacity);
             systemDevicesMock
-                .Setup(mock => mock.PerformScan(
-                    It.IsAny<DNTScanner.Core.ScannerSettings>(),
+                .Setup(mock => mock.ScannerDevices(
                     It.IsAny<IImagingDeviceConfig>(),
-                    It.IsAny<ScanningDevice.ImageFormat>()
+                    It.IsAny<int>()
                     ))
-                .Returns(_images);
-            */
+                .Returns(scannerDevices);
+
+            var scannerSettings = new List<ScannerSettings>();
+            _fixture.AddManyTo(scannerSettings, CollectionMockCapacity);
+            // Ensure there is an item with a predictable ID
+            scannerSettings.First().Id = CollectionMockCapacity.ToString();
+            systemDevicesMock
+                .Setup(mock => mock.ScannerSettings(
+                    It.IsAny<int>()
+                    ))
+                .Returns(scannerSettings);
         }
 
         [TestMethod()]
         public void ScanningDevicesTest()
         {
-            Assert.Fail();
+            var instance = _fixture.Create<ScanningDevices>();
+
+            instance.AssertAllPropertiesAreNotDefault();
+            instance.IsEmpty.Should().BeFalse();
+            instance.Devices.Count().Should().Be(CollectionMockCapacity);
         }
 
         [TestMethod()]
-        public void ScanningDevicesTest1()
+        public void ScanningDevicesGreedyTest()
         {
-            Assert.Fail();
+            _fixture.SetGreedyConstructor<ScanningDevices>();
+            var instance = _fixture.Create<ScanningDevices>();
+
+            instance.AssertAllPropertiesAreNotDefault();
+            instance.IsEmpty.Should().BeFalse();
+            instance.Devices.Count().Should().Be(CollectionMockCapacity);
         }
 
         [TestMethod()]
-        public void AddDeviceTest()
+        public void AddDeviceByIDTest()
         {
-            Assert.Fail();
+            var instance = _fixture.Create<ScanningDevices>();
+            string predictableID = CollectionMockCapacity.ToString();
+
+            // The deviceID parameter is checked against IDs returned by ISystemDevices.ScannerSettings()
+            // The deviceID will not already be in the collection as it is initialised from
+            // SystemDevices.ScannerDevices() which will return mocked devices
+            instance.AddDevice(deviceID: predictableID);
+
+            instance.Devices.Should()
+                .HaveCount(CollectionMockCapacity + 1)
+                .And.OnlyHaveUniqueItems()
+                .And.Contain(i => i.DeviceID == predictableID);
         }
 
         [TestMethod()]
-        public void AddDeviceTest1()
+        public void AddDeviceInstanceTest()
         {
-            Assert.Fail();
+            var instance = _fixture.Create<ScanningDevices>();
+            // Use concrete class otherwise cast from interface proxy type fails
+            var device = _fixture.Create<ScanningDevice>();
+
+            instance.AddDevice(device);
+
+            instance.Devices.Should()
+                .HaveCount(CollectionMockCapacity + 1)
+                .And.Contain(device);
         }
 
         [TestMethod()]
-        public void AddDeviceTest2()
+        public void AddDeviceInstanceRaisesArgumentExceptionTest()
         {
-            Assert.Fail();
-        }
+            var instance = _fixture.Create<ScanningDevices>();
+            // Use concrete class otherwise cast from interface proxy type fails
+            var device = _fixture.Create<ScanningDevice>();
 
-        [TestMethod()]
-        public void AddDeviceTest3()
-        {
-            Assert.Fail();
+            // Ensure the device already exists in the collection
+            instance.AddDevice(device);
+
+            instance
+                .Invoking(i => i.AddDevice(device))
+                .Should().Throw<ArgumentException>();
         }
 
         [TestMethod()]
         public void DisableAllTest()
         {
-            Assert.Fail();
+            var instance = _fixture.Create<ScanningDevices>();
+
+            instance.Devices.All(device => device.IsEnabled == true).Should().BeTrue();
+
+            instance.DisableAll();
+
+            instance.Devices.All(device => device.IsEnabled == false).Should().BeTrue();
         }
 
         [TestMethod()]
         public void EnableAllTest()
         {
-            Assert.Fail();
+            var instance = _fixture.Create<ScanningDevices>();
+
+            foreach (var device in instance.Devices)
+            {
+                device.IsEnabled = false;
+            }
+
+            instance.Devices.All(device => device.IsEnabled == false).Should().BeTrue();
+
+            instance.EnableAll();
+
+            instance.Devices.All(device => device.IsEnabled == true).Should().BeTrue();
         }
 
         [TestMethod()]
-        public void GetByEnabledTest()
+        [DataRow(true)]
+        [DataRow(false)]
+        public void GetByEnabledTest(bool enabled)
         {
-            Assert.Fail();
+            var instance = _fixture.Create<ScanningDevices>();
+            var device = instance.Devices.First();
+
+            device.IsEnabled = false;
+
+            var results = instance.GetByEnabled(enabled);
+
+            if (enabled)
+            {
+                results.Should()
+                    .HaveCount(CollectionMockCapacity - 1)
+                    .And.NotContain(device);
+            }
+            else
+            {
+                results.Should().OnlyContain(d => d == device);
+            }
         }
 
         [TestMethod()]
-        public void RefreshDevicesTest()
+        public void RefreshDevicesDisconnectedAllTest()
         {
-            Assert.Fail();
+            var instance = _fixture.Create<ScanningDevices>();
+
+            // The mock SystemDevices.ScannerProperties() will return nothing
+            // So the existing devices will be marked as disconnected
+            instance.RefreshDevices();
+
+            instance.Devices.All(device => device.IsConnected == false).Should().BeTrue();
         }
 
         [TestMethod()]
-        public void RefreshDevicesTest1()
+        public void RefreshDevicesDisconnectedSingleTest()
         {
-            Assert.Fail();
+            var systemDevices = _fixture.Create<ISystemDevices>();
+            var systemDevicesMock = Mock.Get(systemDevices);
+
+            var instance = _fixture.Create<ScanningDevices>();
+            var initalDevices = instance.Devices;
+
+            // Get the existing DeviceIDs and return from ISystemDevices.ScannerProperties()
+            // Remove one DeviceID so one device will appear disconnected
+            var deviceIDs = instance.Devices.Select(device => device.DeviceID).ToList();
+            var scannerProperties = new List<IDictionary<string, object>>();
+            foreach (var deviceID in deviceIDs.Take(deviceIDs.Count - 1))
+            {
+                scannerProperties.Add(new Dictionary<string, object>() { { "Unique Device ID", deviceID } });
+            }
+            systemDevicesMock
+                .Setup(mock => mock.ScannerProperties(It.IsAny<int>()))
+                .Returns(scannerProperties);
+
+            instance.RefreshDevices();
+
+            // All devices should be connected except the last
+            instance.Devices
+                .Should().HaveCount(CollectionMockCapacity)
+                .And.BeEquivalentTo(initalDevices);
+            instance.Devices
+                .Take(instance.Devices.Count() - 1)
+                .All(device => device.IsConnected == true)
+                .Should().BeTrue();
+            instance.Devices
+                .Last()
+                .IsConnected
+                .Should().BeFalse();
         }
 
         [TestMethod()]
-        public void SetDeviceEnabledTest()
+        [DataRow(true)]
+        [DataRow(false)]
+        public void SetDeviceEnabledTest(bool enabled)
         {
-            Assert.Fail();
+            var instance = _fixture.Create<ScanningDevices>();
+            var device = instance.Devices.First();
+
+            instance.SetDeviceEnabled(device.DeviceID, enabled);
+
+            instance.Devices
+                .Where(d => d.DeviceID == device.DeviceID)
+                .Select(d => d.IsEnabled)
+                .Should().Equal(enabled);
         }
 
         [TestMethod()]
-        public void SetDeviceEnabledTest1()
+        [DataRow(true)]
+        [DataRow(false)]
+        public void SetDeviceEnabledInstanceTest(bool enabled)
         {
-            Assert.Fail();
+            var instance = _fixture.Create<ScanningDevices>();
+            var device = instance.Devices.First();
+
+            instance.SetDeviceEnabled(device, enabled);
+
+            instance.Devices
+                .Where(d => d.DeviceID == device.DeviceID)
+                .Select(d => d.IsEnabled)
+                .Should().Equal(enabled);
         }
     }
 }
