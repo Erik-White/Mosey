78f4126b3d425536789f9f69d92f2b6ce536a425
Erik White <26148654+Erik-White@users.noreply.github.com>
2021-06-21 22:58:33 +0200

    Update note on TimeSpan deserialization

--- Mosey/Services/IntervalTimerConfig.cs
+++ Mosey/Services/IntervalTimerConfig.cs
@@ -6,7 +6,7 @@ namespace Mosey.Services
     public class IntervalTimerConfig : IIntervalTimerConfig
     {
         // System.Text.Json doesn't support TimeSpan [de]serialization
-        // It is planned for .NET Core 5
+        // It is planned for .NET 6
         [System.Text.Json.Serialization.JsonConverter(typeof(System.Text.Json.Serialization.JsonTimeSpanConverter))]
         public TimeSpan Delay { get; set; }
         [System.Text.Json.Serialization.JsonConverter(typeof(System.Text.Json.Serialization.JsonTimeSpanConverter))]
